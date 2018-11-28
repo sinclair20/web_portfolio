164cfb65c30caffce45b6e7dbaad6cf7c2fd5d43
지창호 <sinclar_j@naver.com>
2018-11-28 13:41:41 +0900

    web project portfolio

--- Movie/src/main/java/com/movie/board/comment/DeleteCommentServlet.java
+++ Movie/src/main/java/com/movie/board/comment/DeleteCommentServlet.java
@@ -38,30 +38,7 @@ public class DeleteCommentServlet extends HttpServlet {
 	    
    		
    		}
-   	
-   	public class CommentDeleteAction implements Action
-		{
-		    @Override
-		    public ActionForward execute(HttpServletRequest request,
-		            HttpServletResponse response) throws Exception {
-		    
-			   		int commentID = Integer.parseInt(request.getParameter("commentID"));
-			   		System.out.println("commentIDddd : " + commentID);
-
-			        CommentDAO commentDAO = new CommentDAO();
-			        boolean result = commentDAO.deleteComment(commentID);
-			        
-		        
-		        response.setContentType("text/html;charset=UTF-8");
-		        PrintWriter out = response.getWriter();
-		 
-		        // 정상적으로 댓글을 삭제했을경우 1을 전달한다.
-		        if(result) out.println("1");
-		        
-		        out.close();
-		        return null;
-		    }
-		}
+   
 		
 	}
 
