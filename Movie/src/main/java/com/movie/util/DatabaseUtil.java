4819e8cc0079965891db228d8fa541d49c3d43dd
지창호 <sinclar_j@naver.com>
2018-12-01 20:16:30 +0900

    web

--- Movie/src/main/java/com/movie/util/DatabaseUtil.java
+++ Movie/src/main/java/com/movie/util/DatabaseUtil.java
@@ -7,9 +7,10 @@ public class DatabaseUtil {
 
 	public static Connection getConnection() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/Movie?useTimezone=true&serverTimezone=UTC";
-			String dbID = "root";
-			String dbPassword = "842693";
+			/*String dbURL = "jdbc:mysql://localhost:3306/Movie?useTimezone=true&serverTimezone=UTC";*/
+			String dbURL = "jdbc:mysql://54.180.114.30:3306/Movie?useTimezone=true&serverTimezone=UTC";
+			String dbID = "sinclair";
+			String dbPassword = "842693aa!!";
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
