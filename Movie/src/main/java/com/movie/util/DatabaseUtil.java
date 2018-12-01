7630fd6f242c6dcf28802fb496ff076a404ca635
지창호 <sinclar_j@naver.com>
2018-12-01 20:38:31 +0900

    web

--- Movie/src/main/java/com/movie/util/DatabaseUtil.java
+++ Movie/src/main/java/com/movie/util/DatabaseUtil.java
@@ -8,7 +8,7 @@ public class DatabaseUtil {
 	public static Connection getConnection() {
 		try {
 			/*String dbURL = "jdbc:mysql://localhost:3306/Movie?useTimezone=true&serverTimezone=UTC";*/
-			String dbURL = "jdbc:mysql://54.180.114.30:3306/Movie?useTimezone=true&serverTimezone=UTC";
+			String dbURL = "jdbc:mysql://mydatabase.cf41ikdvjruh.ap-northeast-2.rds.amazonaws.com:3306/Movie?useTimezone=true&serverTimezone=UTC";			
 			String dbID = "sinclair";
 			String dbPassword = "842693aa!!";
 			Class.forName("com.mysql.cj.jdbc.Driver");
